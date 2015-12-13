d63515442d1f35d0fb3e983c17fa610c0307a75e
John Leszczynski <jrleszczynski@gmail.com>
2015-12-13 18:57:57 -0500

    use yeoman pattern

---
+++ src/app/index.constants.js
@@ -0,0 +1,10 @@
+/* global malarkey:false, moment:false */
+(function() {
+  'use strict';
+
+  angular
+    .module('grammarBuilder')
+    .constant('malarkey', malarkey)
+    .constant('moment', moment);
+
+})();
---
+++ src/app/index.module.js
@@ -0,0 +1,7 @@
+(function() {
+  'use strict';
+
+  angular
+    .module('grammarBuilder', ['ngResource', 'ui.router', 'toastr']);
+
+})();
