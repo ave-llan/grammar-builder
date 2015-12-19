5a08fde8c2ce91ace6caa7fbaafe9d7baf92d6c4
John Leszczynski <jrleszczynski@gmail.com>
2015-12-19 07:45:23 -0500

    add sample content

--- src/app.js
+++ src/app.js
@@ -1,9 +1,30 @@
 'use strict';
-
 var angular = require('angular');
+var GrammarGraph = require('grammar-graph');
 
-angular.module('grammarBuilder', ['$scope',
-  function($scope) {
-    $scope.test = 'a test that is working';
-  }
-]);
+var grammarBuilderApp = angular.module('grammarBuilder', []);
+
+grammarBuilderApp.controller('grammarBuilderCtrl',function($scope) {
+  $scope.message = 'this message brought to you from an Angular controller';
+
+  var grammar = {
+          Sentence: ['NounPhrase VerbPhrase'],
+        NounPhrase: ['the Noun',
+                     'the Noun RelativeClause'],
+        VerbPhrase: ['Verb',
+                     'Verb NounPhrase'],
+    RelativeClause: ['that VerbPhrase'],
+              Noun: ['dog',
+                     'cat',
+                     'bird',
+                     'squirrel'],
+              Verb: ['befriended',
+                     'loved',
+                     'ate',
+                     'attacked']
+  };
+  var graph = new GrammarGraph(grammar)
+
+  $scope.vertices = graph.vertices();
+
+});
