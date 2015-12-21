86a237957e7f2abcd13a47cb1a6ba685f785fb50
John Leszczynski <jrleszczynski@gmail.com>
2015-12-20 23:14:53 -0500

    generate (possibly endless) random sentence from sample grammar

--- src/app.js
+++ src/app.js
@@ -4,27 +4,46 @@ var GrammarGraph = require('grammar-graph');
 
 var grammarBuilderApp = angular.module('grammarBuilder', []);
 
-grammarBuilderApp.controller('grammarBuilderCtrl',function($scope) {
+grammarBuilderApp.controller('grammarBuilderCtrl',function($scope, $interval) {
   $scope.message = 'this message brought to you from an Angular controller';
 
   var grammar = {
-          Sentence: ['NounPhrase VerbPhrase'],
-        NounPhrase: ['the Noun',
-                     'the Noun RelativeClause'],
-        VerbPhrase: ['Verb',
-                     'Verb NounPhrase'],
+    Sentence: ['NounPhrase VerbPhrase'],
+    NounPhrase: ['the Noun',
+           'the Noun RelativeClause'],
+    VerbPhrase: ['Verb',
+           'Verb NounPhrase'],
     RelativeClause: ['that VerbPhrase'],
-              Noun: ['dog',
-                     'cat',
-                     'bird',
-                     'squirrel'],
-              Verb: ['befriended',
-                     'loved',
-                     'ate',
-                     'attacked']
+    Noun: ['dog',
+           'cat',
+           'bird',
+           'squirrel'],
+    Verb: ['befriended',
+           'loved',
+           'ate',
+           'attacked']
   };
-  var graph = new GrammarGraph(grammar)
-
+  var graph = new GrammarGraph(grammar);
+  $scope.grammar = grammar;
+  var guide = graph.createGuide('Sentence');
+  $scope.sentence = '';
   $scope.vertices = graph.vertices();
+  function addWord () {
+    if (guide.choices()) {
+      var choices = guide.choices();
+      var choice = choices[Math.floor(Math.random() * choices.length)];
+      guide.choose(choice);
+      $scope.sentence += ' ' + choice;
+    }
+  }
 
+  function generateRandomSentence (guide) {
+    while (guide.choices() && !guide.isComplete()) {
+      var choices = guide.choices();
+      guide.choose(choices[Math.floor(Math.random() * choices.length)]);
+    }
+    return guide.construction().join(' ');
+  }
+
+  $interval(addWord, 400);
 });
