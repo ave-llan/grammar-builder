fca9718c38c27f1638d804855d53a0cd81eb04e1
John Leszczynski <jrleszczynski@gmail.com>
2015-12-23 06:55:44 -0500

    modularize controller

--- src/app/app.module.js
+++ src/app/app.module.js
@@ -1,53 +1,14 @@
 'use strict';
 var angular = require('angular');
-var GrammarGraph = require('grammar-graph');
+
 
 var grammarBuilderApp = angular.module('grammarBuilder', [
   require('angular-ui-router')
 ]);
 
-grammarBuilderApp.controller('BuilderCtrl', function($scope, $interval) {
-  $scope.message = 'this message brought to you from an Angular controller';
+// controllers
+var BuilderCtrl = require('./components/builder/builderctrl.js');
+grammarBuilderApp.controller('BuilderCtrl', BuilderCtrl);
 
-  var grammar = {
-    Sentence: ['NounPhrase VerbPhrase'],
-    NounPhrase: ['the Noun',
-           'the Noun RelativeClause'],
-    VerbPhrase: ['Verb',
-           'Verb NounPhrase'],
-    RelativeClause: ['that VerbPhrase'],
-    Noun: ['dog',
-           'cat',
-           'bird',
-           'squirrel'],
-    Verb: ['befriended',
-           'loved',
-           'ate',
-           'attacked']
-  };
-  var graph = new GrammarGraph(grammar);
-  $scope.grammar = grammar;
-  var guide = graph.createGuide('Sentence');
-  $scope.sentence = '';
-  $scope.vertices = graph.vertices();
-  function addWord () {
-    if (guide.choices()) {
-      var choices = guide.choices();
-      var choice = choices[Math.floor(Math.random() * choices.length)];
-      guide.choose(choice);
-      $scope.sentence += ' ' + choice;
-    }
-  }
-
-  // function generateRandomSentence (guide) {
-  //   while (guide.choices() && !guide.isComplete()) {
-  //     var choices = guide.choices();
-  //     guide.choose(choices[Math.floor(Math.random() * choices.length)]);
-  //   }
-  //   return guide.construction().join(' ');
-  // }
-
-  $interval(addWord, 400);
-});
-
+// config with routes
 grammarBuilderApp.config(require('./app.routes.js'));
---
+++ src/app/components/builder/builderctrl.js
@@ -0,0 +1,49 @@
+var GrammarGraph = require('grammar-graph');
+
+var BuilderCtrl = function($scope, $interval) {
+  $scope.message = 'this message brought to you from an Angular controller';
+
+  var grammar = {
+    Sentence: ['NounPhrase VerbPhrase'],
+    NounPhrase: ['the Noun',
+           'the Noun RelativeClause'],
+    VerbPhrase: ['Verb',
+           'Verb NounPhrase'],
+    RelativeClause: ['that VerbPhrase'],
+    Noun: ['dog',
+           'cat',
+           'bird',
+           'squirrel'],
+    Verb: ['befriended',
+           'loved',
+           'ate',
+           'attacked']
+  };
+  var graph = new GrammarGraph(grammar);
+  $scope.grammar = grammar;
+  var guide = graph.createGuide('Sentence');
+  $scope.sentence = '';
+  $scope.vertices = graph.vertices();
+  function addWord () {
+    if (guide.choices()) {
+      var choices = guide.choices();
+      var choice = choices[Math.floor(Math.random() * choices.length)];
+      guide.choose(choice);
+      $scope.sentence += ' ' + choice;
+    }
+  }
+
+  // function generateRandomSentence (guide) {
+  //   while (guide.choices() && !guide.isComplete()) {
+  //     var choices = guide.choices();
+  //     guide.choose(choices[Math.floor(Math.random() * choices.length)]);
+  //   }
+  //   return guide.construction().join(' ');
+  // }
+
+  $interval(addWord, 400);
+};
+
+BuilderCtrl.name = 'BuilderCtrl';
+
+module.exports = BuilderCtrl;
