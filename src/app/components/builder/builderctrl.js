2a3831550c8315b0f3314d392926f4b7c018c860
John Leszczynski <jrleszczynski@gmail.com>
2015-12-23 20:00:22 -0500

    add placeholder text

--- src/app/components/builder/builderctrl.js
+++ src/app/components/builder/builderctrl.js
@@ -1,9 +1,14 @@
-var GrammarGraph = require('grammar-graph');
+// var GrammarGraph = require('grammar-graph');
 
 var BuilderCtrl = [ '$scope', '$interval',
-  function($scope, $interval) {
+  function($scope /*, $interval */) {
     $scope.message = 'this message brought to you from an Angular controller';
-    var grammar = {};
+    $scope.grammar = {};
+
+    $scope.addNewDefinition = function (symbol, definition) {
+      console.log('you submitted:', symbol, definition);
+    };
+
     // var grammar = {
     //   Sentence: ['NounPhrase VerbPhrase'],
     //   NounPhrase: ['the Noun',
@@ -20,19 +25,19 @@ var BuilderCtrl = [ '$scope', '$interval',
     //          'ate',
     //          'attacked']
     // };
-    var graph = new GrammarGraph(grammar);
-    $scope.grammar = grammar;
-    var guide = graph.createGuide('Sentence');
-    $scope.sentence = '';
-    $scope.vertices = graph.vertices();
-    function addWord () {
-      if (guide.choices()) {
-        var choices = guide.choices();
-        var choice = choices[Math.floor(Math.random() * choices.length)];
-        guide.choose(choice);
-        $scope.sentence += ' ' + choice;
-      }
-    }
+    // var graph = new GrammarGraph(grammar);
+    // $scope.grammar = grammar;
+    // var guide = graph.createGuide('Sentence');
+    // $scope.sentence = '';
+    // $scope.vertices = graph.vertices();
+    // function addWord () {
+    //   if (guide.choices()) {
+    //     var choices = guide.choices();
+    //     var choice = choices[Math.floor(Math.random() * choices.length)];
+    //     guide.choose(choice);
+    //     $scope.sentence += ' ' + choice;
+    //   }
+    // }
 
     // function generateRandomSentence (guide) {
     //   while (guide.choices() && !guide.isComplete()) {
