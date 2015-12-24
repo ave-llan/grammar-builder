68524975494bcf9b906f44b5153ef33294a373e7
John Leszczynski <jrleszczynski@gmail.com>
2015-12-23 22:28:41 -0500

    select start symbol and make it green

--- src/app/components/builder/builderctrl.js
+++ src/app/components/builder/builderctrl.js
@@ -4,19 +4,26 @@ var BuilderCtrl = [ '$scope', '$interval',
   function($scope /*, $interval */) {
     $scope.message = 'this message brought to you from an Angular controller';
     $scope.grammar = {};
+    $scope.startSymbol;      // where should guides start their construction?
+    // var graph;            // a grammarGraph made with current grammar
 
+    // add a new definition to grammar and refresh guide
     $scope.addNewDefinition = function (symbol, definition) {
       if (!(symbol && definition)) return;
       if (!(symbol in $scope.grammar))
         $scope.grammar[symbol] = [];
       $scope.grammar[symbol].push(definition);
       $scope.definition = ''; // clear definition but leave symbol
+
+      // graph = new GrammarGraph($scope.grammar); // refresh guide
+      // guide = graph.createGuide()
     };
 
     // is the given symbol a non-terminal?
     $scope.isNonTerminal = function (symbol) {
       return (symbol in $scope.grammar);
     };
+
 
 
 
