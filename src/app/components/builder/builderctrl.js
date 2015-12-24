5f60bd7c0648333d1d68ce34a032ac45eae9a5c3
John Leszczynski <jrleszczynski@gmail.com>
2015-12-23 22:43:26 -0500

    generate sample sentence from current grammar

--- src/app/components/builder/builderctrl.js
+++ src/app/components/builder/builderctrl.js
@@ -1,23 +1,28 @@
-// var GrammarGraph = require('grammar-graph');
+var GrammarGraph = require('grammar-graph');
 
 var BuilderCtrl = [ '$scope', '$interval',
-  function($scope /*, $interval */) {
+  function($scope , $interval ) {
     $scope.message = 'this message brought to you from an Angular controller';
     $scope.grammar = {};
     $scope.startSymbol;      // where should guides start their construction?
-    // var graph;            // a grammarGraph made with current grammar
+    $scope.sentence;         // a sample sentence from this grammar
+    var graph;               // a grammarGraph made with current grammar
+    var guide;               // the current guide
+    var sentenceInterval;    // sentence refresh promosie
 
     // add a new definition to grammar and refresh guide
     $scope.addNewDefinition = function (symbol, definition) {
       if (!(symbol && definition)) return;
       if (!$scope.startSymbol) $scope.startSymbol = symbol; // initialize
       if (!(symbol in $scope.grammar))
-        $scope.grammar[symbol] = [];
+        $scope.grammar[symbol] = [];                        // initialize this non-terminal
       $scope.grammar[symbol].push(definition);
-      $scope.definition = ''; // clear definition but leave symbol
+      $scope.definition = '';                               // clear definition but leave symbol
 
-      // graph = new GrammarGraph($scope.grammar); // refresh guide
-      // guide = graph.createGuide()
+      graph = new GrammarGraph($scope.grammar); // refresh guide
+      guide = graph.createGuide($scope.startSymbol);
+
+      generateRandomSentence();
     };
 
     // is the given symbol a non-terminal?
@@ -49,22 +54,20 @@ var BuilderCtrl = [ '$scope', '$interval',
     // var guide = graph.createGuide('Sentence');
     // $scope.sentence = '';
     // $scope.vertices = graph.vertices();
-    // function addWord () {
-    //   if (guide.choices()) {
-    //     var choices = guide.choices();
-    //     var choice = choices[Math.floor(Math.random() * choices.length)];
-    //     guide.choose(choice);
-    //     $scope.sentence += ' ' + choice;
-    //   }
-    // }
+    function addWord () {
+      if (guide.choices()) {
+        var choices = guide.choices();
+        var choice = choices[Math.floor(Math.random() * choices.length)];
+        guide.choose(choice);
+        $scope.sentence += ' ' + choice;
+      }
+    }
 
-    // function generateRandomSentence (guide) {
-    //   while (guide.choices() && !guide.isComplete()) {
-    //     var choices = guide.choices();
-    //     guide.choose(choices[Math.floor(Math.random() * choices.length)]);
-    //   }
-    //   return guide.construction().join(' ');
-    // }
+    function generateRandomSentence () {
+      $scope.sentence = '';
+      $interval.cancel(sentenceInterval);
+      $interval(addWord, 1000);
+    }
 
     // $interval(addWord, 400);
   }
