77422a4c9eb3e7d4ffdad14ad5c0292283013503
John Leszczynski <jrleszczynski@gmail.com>
2015-12-28 21:42:07 -0500

    require inside ctrl declaration

--- src/app/app.module.js
+++ src/app/app.module.js
@@ -1,14 +1,14 @@
 'use strict';
 var angular = require('angular');
 
-
 var grammarBuilderApp = angular.module('grammarBuilder', [
   require('angular-ui-router')
 ]);
 
 // controllers
-var BuilderCtrl = require('./components/builder/builderctrl.js');
-grammarBuilderApp.controller('BuilderCtrl', BuilderCtrl);
+grammarBuilderApp.controller('BuilderCtrl', require('./components/builder/builderctrl.js'));
+
+// directives
 
 // config with routes
 grammarBuilderApp.config(require('./app.routes.js'));
