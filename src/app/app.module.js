5db74301bd6b1b467cbfb6182460148c50cf4bf1
John Leszczynski <jrleszczynski@gmail.com>
2015-12-28 22:09:17 -0500

    add definiton directive

--- src/app/app.module.js
+++ src/app/app.module.js
@@ -9,6 +9,7 @@ var grammarBuilderApp = angular.module('grammarBuilder', [
 grammarBuilderApp.controller('BuilderCtrl', require('./components/builder/builderctrl.js'));
 
 // directives
+grammarBuilderApp.directive('definition', require('./shared/definition/definition_dir.js'));
 
 // config with routes
 grammarBuilderApp.config(require('./app.routes.js'));
