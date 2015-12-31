4e965e778be8a14dda249add7f519df9ec70c13a
John Leszczynski <jrleszczynski@gmail.com>
2015-12-31 06:55:38 -0500

    create a whole module for builder and require it as a dependency for the app

--- src/app/app.module.js
+++ src/app/app.module.js
@@ -1,12 +1,19 @@
 'use strict';
-var angular = require('angular');
+var angular = require('angular')
+  , builder = require('./components/builder/builderctrl.js');
+
+console.log('builder.name:', builder.name);
+/**
+ * @see https://google.github.io/styleguide/angularjs-google-style.html
+ */
 
 var grammarBuilderApp = angular.module('grammarBuilder', [
-  require('angular-ui-router')
+  require('angular-ui-router'),
+  builder.name
 ]);
 
 // controllers
-grammarBuilderApp.controller('BuilderCtrl', require('./components/builder/builderctrl.js'));
+// grammarBuilderApp.controller('BuilderCtrl', require('./components/builder/builderctrl.js'));
 
 // directives
 grammarBuilderApp.directive('definition', require('./shared/definition/definition_dir.js'));
