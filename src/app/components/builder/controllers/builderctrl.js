a85fcc0b28066e5f84b11993d7a5720830df6eeb
John Leszczynski <jrleszczynski@gmail.com>
2016-01-26 20:55:06 -0500

    add editable title

--- src/app/components/builder/controllers/builderctrl.js
+++ src/app/components/builder/controllers/builderctrl.js
@@ -4,6 +4,7 @@ var BuilderCtrl = [ '$scope', '$interval', '$http', 'grammarFactory',
   function($scope , $interval, $http, grammarFactory) {
     console.log(grammarFactory);
     $scope.grammar = {};
+    $scope.title = '';
 
     // for now, pull sample grammar
     $http.get('./assets/data/grammars/sample-grammar.json').then(function (response) {
