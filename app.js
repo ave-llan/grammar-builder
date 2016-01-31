1330f424efbffabb28856790ca7f5cdbb1469784
John Leszczynski <jrleszczynski@gmail.com>
2016-01-31 11:11:41 -0500

    remove comments

--- app.js
+++ app.js
@@ -4,17 +4,13 @@ var express = require('express');
 
 var app = express();
 
-
-// Say hello!
 app.get('/', function(req, res) {
   res.sendFile('dist/index.html', { root: __dirname });
-  // res.status(200).send('Hello, world!');
 });
 
 app.use(express.static('dist'));
 
 
-// [START server]
 // Start the server
 var server = app.listen(process.env.PORT || 8080, function () {
   var host = server.address().address;
@@ -22,4 +18,3 @@ var server = app.listen(process.env.PORT || 8080, function () {
 
   console.log('App listening at http://%s:%s', host, port);
 });
-// [END server]
