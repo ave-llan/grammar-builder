7e23a346ebf27fb6321b55acb9328dd5b97d44b9
John Leszczynski <jrleszczynski@gmail.com>
2016-01-31 12:46:21 -0500

    prepare for oauth

--- app.js
+++ app.js
@@ -1,15 +1,21 @@
 'use strict';
 
-var express = require('express');
+var express = require('express')
+  , session = require('cookie-session')
+  , config  = require('./config');
 
 var app = express();
+
+app.use(session({
+  secret: config.secret,
+  signed: true
+}));
 
 app.get('/', function(req, res) {
   res.sendFile('dist/index.html', { root: __dirname });
 });
 
 app.use(express.static('dist'));
-
 
 // Start the server
 var server = app.listen(process.env.PORT || 8080, function () {
